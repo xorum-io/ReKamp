1c00e8c503209ec1a2a87e4233498216b9690537
Yevhenii Kanivets <yevhenii.kanivets@gmail.com>
2020-05-07 10:27:41 +0300

    Initial commit.

---
+++ src/commonMain/kotlin/tw/geothings/rekotlin/Store.kt
@@ -0,0 +1,162 @@
+package tw.geothings.rekotlin
+
+/**
+ * Created by Taras Vozniuk on 31/07/2017.
+ * Copyright © 2017 GeoThings. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+class Store<State: StateType> (
+        private val reducer: Reducer<State>,
+        state: State?,
+        middleware: List<Middleware<State>> = emptyList(),
+        automaticallySkipRepeats: Boolean = true): StoreType<State> {
+
+    private var _state: State? = state
+        set(value) {
+            val oldValue = field
+            field = value
+
+            value?.let {
+                subscriptions.forEach {
+                    it.newValues(oldValue, value)
+                }
+            }
+        }
+
+    override val state: State
+        get() { return _state!! }
+
+    @Suppress("NAME_SHADOWING")
+    override var dispatchFunction: DispatchFunction = middleware
+            .reversed()
+            .fold({ action: Action -> this._defaultDispatch(action) }, { dispatchFunction, middleware ->
+                val dispatch = { action: Action -> this.dispatch(action) }
+                val getState = { this._state }
+                middleware(dispatch, getState)(dispatchFunction)
+            })
+
+    val subscriptions: MutableList<SubscriptionBox<State, Any>> = mutableListOf()
+
+    private var isDispatching = false
+
+    /**
+     * Indicates if new subscriptions attempt to apply `skipRepeats` by default.
+     */
+    val subscribersAutomaticallySkipsRepeat: Boolean = automaticallySkipRepeats
+
+    init {
+        this._state?.let { this._state = state } ?: this.dispatch(ReKotlinInit())
+    }
+
+    override fun <S: StoreSubscriber<State>> subscribe(subscriber: S) {
+
+        // if subscribersAutomaticallySkipsRepeat is set
+        // skipRepeats will be applied with kotlin structural equality
+        if (subscribersAutomaticallySkipsRepeat){
+            this.subscribe(subscriber, {
+                it.skipRepeats()
+            })
+        } else {
+            this.subscribe(subscriber, null)
+        }
+    }
+
+    override fun <SelectedState: Any, S: StoreSubscriber<SelectedState>> subscribe(subscriber: S, transform: ((Subscription<State>) -> Subscription<SelectedState>)?) {
+        // If the same subscriber is already registered with the store, replace the existing
+        // subscription with the new one.
+        val index = this.subscriptions.indexOfFirst { it.subscriber == subscriber }
+        if (index != -1){
+            this.subscriptions.removeAt(index)
+        }
+
+        // Create a subscription for the new subscriber.
+        val originalSubscription = Subscription<State>()
+        // Call the optional transformation closure. This allows callers to modify
+        // the subscription, e.g. in order to subselect parts of the store's state.
+        val transformedSubscription = transform?.invoke(originalSubscription)
+
+        val subscriptionBox = SubscriptionBox(originalSubscription, transformedSubscription, subscriber)
+
+        // each subscriber has its own potentially different SelectedState that doesn't have to conform to StateType
+        @Suppress("UNCHECKED_CAST")
+        this.subscriptions.add(subscriptionBox as SubscriptionBox<State, Any>)
+
+        this._state?.let {
+            originalSubscription.newValues(null, it)
+        }
+    }
+
+    override fun <SelectedState: Any> unsubscribe(subscriber: StoreSubscriber<SelectedState>) {
+        val index = this.subscriptions.indexOfFirst { it.subscriber == subscriber }
+        if (index != -1){
+            this.subscriptions.removeAt(index)
+        }
+    }
+
+    fun _defaultDispatch(action: Action){
+        if (isDispatching) {
+            throw Exception(
+                    "ReKotlin:ConcurrentMutationError- Action has been dispatched while" +
+                    " a previous action is action is being processed. A reducer" +
+                    " is dispatching an action, or ReKotlin is used in a concurrent context" +
+                    " (e.g. from multiple threads)."
+            )
+        }
+
+        this.isDispatching = true
+        val newState = reducer(action, this._state)
+        this.isDispatching = false
+
+        this._state = newState
+    }
+
+    override fun dispatch(action: Action){
+        this.dispatchFunction(action)
+    }
+
+    override fun dispatch(actionCreator: ActionCreator<State, StoreType<State>>){
+        actionCreator(this.state, this)?.let {
+            this.dispatch(it)
+        }
+    }
+
+    override fun dispatch(asyncActionCreator: AsyncActionCreator<State, StoreType<State>>){
+        this.dispatch(asyncActionCreator, null)
+    }
+
+    override fun dispatch(asyncActionCreator: AsyncActionCreator<State, StoreType<State>>, callback: DispatchCallback<State>?){
+        asyncActionCreator(this.state, this) { actionProvider ->
+            val action = actionProvider(this.state, this)
+
+            action?.let {
+                this.dispatch(it)
+                callback?.invoke(this.state)
+            }
+        }
+    }
+}
+
+typealias DispatchCallback<State> = (State) -> Unit
+
+typealias ActionCreator<State, Store> = (state: State, store: Store) -> Action?
+
+typealias AsyncActionCreator<State, Store> = (state: State, store: Store, actionCreatorCallback: (ActionCreator<State, Store>) -> Unit) -> Unit
---
+++ src/commonMain/kotlin/tw/geothings/rekotlin/Subscription.kt
@@ -0,0 +1,190 @@
+package tw.geothings.rekotlin
+
+/**
+ * Created by Taras Vozniuk on 07/08/2017.
+ * Copyright © 2017 GeoThings. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * A box around subscriptions and subscribers.
+ *
+ * Acts as a type-erasing wrapper around a subscription and its transformed subscription.
+ * The transformed subscription has a type argument that matches the selected substate of the
+ * subscriber; however that type cannot be exposed to the store.
+ *
+ * The box subscribes either to the original subscription, or if available to the transformed
+ * subscription and passes any values that come through this subscriptions to the subscriber.
+ *
+ */
+class SubscriptionBox<State, SelectedState: Any>(val originalSubscription: Subscription<State>,
+                             transformedSubscription: Subscription<SelectedState>?,
+                             val subscriber: StoreSubscriber<SelectedState>) where State: StateType {
+
+    // hoping to mimic swift weak reference
+    // however this doesn't really work the same way, gc collects non-deterministically
+    // setting the original subscriber to null will not result in this being nulled synchronously
+    /*
+    var _subscriber: WeakReference<StoreSubscriber<SelectedState>> = WeakReference(subscriber)
+    val subscriber: StoreSubscriber<SelectedState>?
+        get() {
+            return _subscriber.get()
+        }
+    */
+
+    init {
+        // If we haven't received a transformed subscription, we forward all values
+        // from the original subscription.
+        val forwardFromOriginalSubscription = {
+            // original Swift implementation has type errased subscriber
+            // to avoid casting and passing incompatible value
+            // conditional cast was added check
+            originalSubscription.observe { _, newState ->
+                @Suppress("UNCHECKED_CAST")
+                (newState as? SelectedState)?.let {
+                    this.subscriber.newState(it)
+                }
+            }
+        }
+
+        // If we received a transformed subscription, we subscribe to that subscription
+        // and forward all new values to the subscriber.
+        transformedSubscription?.let {
+            transformedSubscription.observe { _, newState ->
+                this.subscriber.newState(newState)
+            }
+
+        } ?: forwardFromOriginalSubscription()
+    }
+
+    fun newValues(oldState: State?, newState: State){
+        // We pass all new values through the original subscription, which accepts
+        // values of type `<State>`. If present, transformed subscriptions will
+        // receive this update and transform it before passing it on to the subscriber.
+        this.originalSubscription.newValues(oldState, newState)
+    }
+}
+
+class Subscription<State: Any> {
+
+    private fun <Substate: Any> _select(selector: ((State) -> Substate)): Subscription<Substate> {
+        return Subscription { sink ->
+            this.observe { oldState, newState ->
+                sink(oldState?.let(selector), selector(newState))
+            }
+        }
+    }
+
+    // region: Public interface
+
+    /**
+     * Provides a subscription that selects a substate of the state of the original subscription.
+     * @param selector A closure that maps a state to a selected substate
+     */
+    fun <Substate: Any> select(selector: ((State) -> Substate)): Subscription<Substate> {
+        return this._select(selector)
+    }
+
+    /**
+     * Provides a subscription that skips certain state updates of the original subscription.
+     * @param isRepeat A closure that determines whether a given state update is a repeat and
+     * thus should be skipped and not forwarded to subscribers.
+     *
+     */
+    fun skipRepeats(isRepeat: (oldState: State, newState: State) -> Boolean): Subscription<State>{
+        return Subscription { sink ->
+            this.observe { oldState, newState ->
+                oldState?.let {
+                    if (!isRepeat(oldState, newState)){
+                        sink(oldState, newState)
+                    }
+
+                } ?: sink(oldState, newState)
+            }
+        }
+    }
+
+    /**
+     * Provides a subscription that skips repeated updates of the original subscription
+     * Repeated updates determined by structural equality
+     */
+    fun skipRepeats(): Subscription<State>{
+        return this.skipRepeats { oldState, newState ->
+            oldState == newState
+        }
+    }
+
+    /** Provides a subscription that skips certain state updates of the original subscription.
+     *
+     * This is identical to `skipRepeats` and is provided simply for convenience.
+     * @param when A closure that determines whether a given state update is a repeat and
+     * thus should be skipped and not forwarded to subscribers.
+     */
+    fun skip(`when`: (oldState: State, newState: State) -> Boolean): Subscription<State>{
+        return this.skipRepeats(`when`)
+    }
+
+    /**
+     * Provides a subscription that only updates for certain state changes.
+     *
+     * This is effectively the inverse of skipRepeats(:)
+     * @param whenBlock A closure that determines whether a given state update should notify
+     */
+    fun only(whenBlock: (oldState: State, newState: State) -> Boolean): Subscription<State>{
+        return this.skipRepeats { oldState, newState ->
+            !whenBlock(oldState, newState)
+        }
+    }
+
+    // endregion
+
+    // region: Internals
+    var observer: ((State?, State) -> Unit)? = null
+
+    init {}
+    constructor()
+
+    /// Initializes a subscription with a sink closure. The closure provides a way to send
+    /// new values over this subscription.
+    private constructor(sink: ((State?, State) -> Unit) -> Unit){
+        // Provide the caller with a closure that will forward all values
+        // to observers of this subscription.
+
+        sink { old, new ->
+            this.newValues(old, new)
+        }
+    }
+
+    /**
+     * Sends new values over this subscription. Observers will be notified of these new values.
+     */
+    fun newValues(oldState: State?, newState: State){
+        this.observer?.invoke(oldState, newState)
+    }
+
+    /// A caller can observe new values of this subscription through the provided closure.
+    /// - Note: subscriptions only support a single observer.
+    internal fun observe(observer: (State?, State) -> Unit){
+        this.observer = observer
+    }
+
+    // endregion
+}
